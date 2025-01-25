766b4d462bb64ff1a514602030fda96b29a90116
yuzuyyy <hendriawan0720@gmail.com>
2025-01-25 13:57:17 +0800

    commit

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -10,7 +10,7 @@ import { hero } from '../assets';
         </div>
         <div class="w-full flex flex-col items-center gap-[60px] z-20">
             <!-- text-content start-->
-            <div class="flex flex-col items-center gap-6">
+            <div class="flex flex-col items-center gap-8">
                 <div class="daisy-btn rounded-full flex items-center gap-1 bg-white/40 shadow shadow-white border-none backdrop-blur-3xl"> <div class="size-2 bg-black rounded-full"></div> Trusted by 20,000+ Happy Learners</div>
 
                 <div class="flex flex-col items-center  gap-8">
@@ -18,7 +18,7 @@ import { hero } from '../assets';
                     <p class="p2 text-center max-w-3xl">Practical project-based courses that are easy to understand, straight to the point, and distractions while ensuring comprehensive learning.</p>
                 </div>
 
-                <div class="flex gap-6">
+                <div class="flex gap-8">
                     <button class="daisy-btn daisy-btn-wide text-xl rounded-full bg-color6/40 backdrop-blur-3xl capitalize border-none shadow-sm shadow-white">View all courses</button>
                     <button class="daisy-btn daisy-btn-wide  outline outline-8 outline-white/70 bg-white text-xl capitalize rounded-full">Start learning now</button>
                 </div>
