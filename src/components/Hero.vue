30232b582a2bd72126e078719d93ab6f23b4fa40
yuzuyyy <hendriawan0720@gmail.com>
2025-01-25 14:32:53 +0800

    commit

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,13 +4,13 @@ import { hero } from '../assets';
 </script>
 
 <template>
-    <section class="relative bg-white flex flex-col items-center pt-[80px] px-[60px] radial ">
-        <div class="absolute inset-0 backdrop-blur-3xl">
+    <section class="relative bg-white flex flex-col items-center pt-[80px] px-[60px] radial">
+        <div class="absolute inset-0 bg-white/30 backdrop-blur-3xl">
 
         </div>
         <div class="w-full flex flex-col items-center gap-[60px] z-20">
             <!-- text-content start-->
-            <div class="flex flex-col items-center gap-8">
+            <div class="flex flex-col items-center gap-8 ">
                 <div class="daisy-btn rounded-full flex items-center gap-1 bg-white/40 shadow shadow-white border-none backdrop-blur-3xl"> <div class="size-2 bg-black rounded-full"></div> Trusted by 20,000+ Happy Learners</div>
 
                 <div class="flex flex-col items-center  gap-8">
@@ -26,7 +26,7 @@ import { hero } from '../assets';
             <!-- text-content end-->
 
             <!-- visual content start -->
-            <div class="relative w-full flex items-center justify-center h-[356px] z-20">
+            <div class="relative w-full flex items-center justify-center h-[356px]">
                 <img 
                 class="absolute max-w-screen-lg h-[428px] min-hh-[500px]"
                 :src="hero" alt="hero">
