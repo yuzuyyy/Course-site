1713e8852f4a9135664cec3fbd5627cf9a5698f6
yuzuyyy <hendriawan0720@gmail.com>
2025-01-25 13:55:52 +0800

    commit

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,11 +4,14 @@ import { hero } from '../assets';
 </script>
 
 <template>
-    <section class="relative bg-white flex flex-col items-center pt-[80px] px-[60px]">
-        <div class="w-full flex flex-col items-center gap-[60px]">
+    <section class="relative bg-white flex flex-col items-center pt-[80px] px-[60px] radial ">
+        <div class="absolute inset-0 backdrop-blur-3xl">
+
+        </div>
+        <div class="w-full flex flex-col items-center gap-[60px] z-20">
             <!-- text-content start-->
             <div class="flex flex-col items-center gap-6">
-                <button class="daisy-btn rounded-full flex items-center gap-1"> <div class="size-2 bg-black rounded-full"></div> Trusted by 20,000+ Happy Learners</button>
+                <div class="daisy-btn rounded-full flex items-center gap-1 bg-white/40 shadow shadow-white border-none backdrop-blur-3xl"> <div class="size-2 bg-black rounded-full"></div> Trusted by 20,000+ Happy Learners</div>
 
                 <div class="flex flex-col items-center  gap-8">
                     <h1 class="heading1 max-w-6xl"> Web Dev & Design made Simple, Better.</h1>
@@ -16,14 +19,14 @@ import { hero } from '../assets';
                 </div>
 
                 <div class="flex gap-6">
-                    <button class="daisy-btn text-xl rounded-full">View all courses</button>
-                    <button class="daisy-btn text-xl rounded-full">Start learning now</button>
+                    <button class="daisy-btn daisy-btn-wide text-xl rounded-full bg-color6/40 backdrop-blur-3xl capitalize border-none shadow-sm shadow-white">View all courses</button>
+                    <button class="daisy-btn daisy-btn-wide  outline outline-8 outline-white/70 bg-white text-xl capitalize rounded-full">Start learning now</button>
                 </div>
             </div>
             <!-- text-content end-->
 
             <!-- visual content start -->
-            <div class="relative w-full flex items-center justify-center h-[356px] bg-black">
+            <div class="relative w-full flex items-center justify-center h-[356px] z-20">
                 <img 
                 class="absolute max-w-screen-lg h-[428px] min-hh-[500px]"
                 :src="hero" alt="hero">
