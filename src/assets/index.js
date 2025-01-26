274601ac8346df5548280860e8888aa3efc9a18a
yuzuyyy <hendriawan0720@gmail.com>
2025-01-26 11:07:51 +0800

    commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,6 +5,12 @@ import visual from './visual.svg'
 import card1 from './card1.png'
 import card2 from './card2.png'
 import card3 from './card3.png'
+import company1 from './company1.png'
+import company2 from './company2.png'
+import company3 from './company3.png'
+import company4 from './company4.png'
+import company5 from './company5.png'
+import company6 from './company6.png'
 export {
     hero,
     logo,
@@ -12,5 +18,11 @@ export {
     visual,
     card1,
     card2,
-    card3
+    card3,
+    company1,
+    company2,
+    company3,
+    company4,
+    company5,
+    company6
 }
