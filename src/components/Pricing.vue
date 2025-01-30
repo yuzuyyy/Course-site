973a167ebf79570083c35a926b7236454f6ca0c8
yuzuyyy <hendriawan0720@gmail.com>
2025-01-30 17:20:05 +0800

    commit

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -10,7 +10,7 @@ import { plans } from '../constans';
         <div class="mx-auto max-w-screen-xl flex flex-col items-center justify-center gap-[60px]">
             <!-- heading start -->
             <div class="w-full flex justify-between items-end max-md:flex-col max-md:items-center max-md:gap-8">
-                <h2 class="heading2 md:text-[42px] font-medium max-md:text-3xl">Join Our Premium Courses</h2>
+                <h2 class="heading2 md:text-[42px] font-medium max-xl:text-3xl">Join Our Premium Courses</h2>
 
                 <div class="flex flex-col gap-6 max-md:items-center max-md:gap-2">
                     <h4 class="heading4 text-xl">Trusted by 70k+ students</h4>
