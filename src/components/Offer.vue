bf5b1d87ac6dc0927e9606b4d64ea2ef57e3d203
yuzuyyy <hendriawan0720@gmail.com>
2025-01-30 18:04:54 +0800

    commit

--- src/components/Offer.vue
+++ src/components/Offer.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { visual } from '../assets';
-import { Badge, CourseCard, LogoSlider } from '../components';
+import { Badge, CourseCard } from '../components';
 import { cards } from '../constans';
 
 
@@ -8,39 +8,38 @@ import { cards } from '../constans';
 </script>
 
 <template>
-    <section class=" section-padding bg-white z-20">
-        <div class="mx-auto max-w-screen-xl flex flex-col  justify-center gap-[44px]">
+    <section class="section-padding bg-white">
+        <div class="mx-auto max-w-screen-xl flex flex-col items-center justify-center gap-[44px]">
             <!-- heading start -->
-            <div class="flex  items-center justify-center gap-[30px] ">
-                <img class="size-[25px] max-md:hidden" :src="visual" alt="">
+            <div class="flex items-center justify-center gap-[30px]">
+                <img class="size-[25px] max-md:hidden" :src="visual" alt="Visual Icon">
 
                 <div class="flex flex-col items-center max-w-[620px] gap-[16px] max-md:max-w-2xl">
                     <Badge title="we offer" />
                     <h2 class="heading2 text-center md:text-[42px] font-medium">Boost Your Skills</h2>
-                    <p class="p1 text-center  ">From critical skills to technical topics, we support your professional
+                    <p class="p1 text-center">From critical skills to technical topics, we support your professional
                         development with courses that help you grow and succeed.</p>
                 </div>
 
-                <img class="size-[25px] max-md:hidden" :src="visual" alt="">
+                <img class="size-[25px] max-md:hidden" :src="visual" alt="Visual Icon">
             </div>
             <!-- heading end -->
 
             <!-- site info start -->
-            <div class="grid-offer ">
+            <div class="grid-offer">
                 <CourseCard v-for="card in cards" :key="card.text" v-bind="card" />
             </div>
             <!-- site info end -->
 
             <!-- social proof start -->
-            <div class="flex flex-col items-center justify-center gap-[16px] w-full ">
-
+            <div class="flex flex-col items-center justify-center gap-[16px] w-full">
                 <div class="flex items-center justify-center w-full">
-                    <div class="h-[0.5px] w-1/4  bg-gray-400">
-                    </div>
+                    <div class="h-[0.5px] w-1/4 bg-gray-400"></div>
                     <p class="p1 text-center w-2/4 max-md:text-sm">Adopted by renowned enterprises such as</p>
                     <div class="h-[0.5px] flex-1 bg-gray-400"></div>
                 </div>
 
+                <!-- Uncomment jika LogoSlider akan digunakan -->
                 <!-- <LogoSlider /> -->
             </div>
             <!-- social proof end -->
