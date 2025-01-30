e5fec67fe71f3f06e432eeb9ed0219c8ede6b7e7
yuzuyyy <hendriawan0720@gmail.com>
2025-01-30 17:42:23 +0800

    commit

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -7,7 +7,7 @@ import {navLinks} from '../constans/'
 </script>
 
 <template>
-    <nav class="bg-transparent top-0 z-50  w-full overflow-hidden fixed top-0 mx-auto bg-white/10 backdrop-blur-3xl">
+    <nav class=" z-50  w-full overflow-hidden fixed top-0 mx-auto bg-white/10 backdrop-blur-3xl">
         <div class="daisy-navbar  flex justify-between py-[16px] px-[50px] max-w-screen-xl mx-auto max-md:px-2">
             <img class="h-[34px] w-[150px] " :src="logo" alt="">
             <div class="flex gap-20 max-md:hidden ">
